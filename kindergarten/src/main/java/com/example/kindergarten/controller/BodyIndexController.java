2ac001d72644a7a972a594661280f342b8978698
mustafaakburak <onlysecretgame@gmail.com>
2021-11-20 00:55:25 +0300

    backend/106 - controller class name refactor

--- kindergarten/src/main/java/com/example/kindergarten/controller/BodyIndexController.java
+++ kindergarten/src/main/java/com/example/kindergarten/controller/BodyIndexController.java
@@ -1,45 +1,43 @@
 package com.example.kindergarten.controller;
 
-import com.example.kindergarten.exception.model.NotFoundException;
 import com.example.kindergarten.model.BodyIndex;
 import com.example.kindergarten.service.BodyIndexService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import javax.xml.ws.Response;
 import java.util.List;
 
 @RestController
-@RequestMapping("/studentinformation")
-public class StudentInformationController {
+@RequestMapping("/bodyIndex")
+public class BodyIndexController {
 
     private BodyIndexService bodyIndexService;
 
-    public StudentInformationController(BodyIndexService bodyIndexService) {
+    public BodyIndexController(BodyIndexService bodyIndexService) {
         this.bodyIndexService = bodyIndexService;
     }
 
-    @PostMapping("/save/bodyIndex")
+    @PostMapping("/save")
     public ResponseEntity<Void> saveBodyIndex(@RequestBody BodyIndex bodyIndex) {
         bodyIndexService.saveBodyIndex(bodyIndex);
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
-    @PostMapping("/delete/bodyIndex")
+    @PostMapping("/delete")
     public ResponseEntity<Void> deleteBodyIndex(@RequestParam String id) {
         bodyIndexService.deleteBodyIndex(id);
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
-    @PutMapping("/update/bodyIndex")
+    @PutMapping("/update")
     public ResponseEntity<Void> updateBodyIndex(@RequestParam String id,
                                                 @RequestBody BodyIndex bodyIndex) {
         bodyIndexService.updateBodyIndex(id, bodyIndex);
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
-    @GetMapping("/list/bodyIndex")
+    @GetMapping("/list")
     public List<BodyIndex> getStudentBodyIndex(@RequestParam String studentId) {
         return bodyIndexService.getStudentBodyIndex(studentId);
     }
